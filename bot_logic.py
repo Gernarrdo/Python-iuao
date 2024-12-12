188b0fd1ce85c2464149f37eead758f64cbfc0ef
Gernarrdo <valtemaraluca@gmail.com>
2024-12-12 15:41:27 +0100

    Update bot_logic.py
    aggiunta funzioni al bot

--- bot_logic.py
+++ bot_logic.py
@@ -29,3 +29,30 @@ def funzioneandrea():
     else:
         print("Ciao Andrea")
         return
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
